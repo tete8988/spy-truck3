454248464ad4b8c99c50ee40ac9a3b0a98bf4332
tete8988 <tetee8988@gmail.com>
2025-05-06 13:30:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SpyTruckDashboard from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <SpyTruckDashboard />
+  </React.StrictMode>
+);
